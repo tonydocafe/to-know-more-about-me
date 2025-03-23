d0744207246d5ddfcc33d35e2bc6e10aa1db599f
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-03-23 16:01:09 -0300

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -5,3 +5,25 @@ function toggleLanguage() {
 }
 
 
+
+function searchText() {
+    const input = document.querySelector('.search-bar').value.toLowerCase();
+    const skills = document.querySelectorAll('.skill');
+    const counter = document.getElementById('searchCounter');
+
+    currentMatchIndex = -1;
+    allMatches = [];
+    
+    skills.forEach(skill => highlightMatches(skill, input));
+
+    totalMatches = allMatches.length;
+    
+    if(totalMatches > 0) {
+        currentMatchIndex = 0;
+        highlightCurrentMatch();
+        counter.style.display = 'block';
+        updateCounter();
+    } else {
+        counter.style.display = 'none';
+    }
+}
