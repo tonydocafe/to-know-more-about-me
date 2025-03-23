d2b5b97f8eedb01e9c9a8b4998332f4644b0234d
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-03-23 15:14:09 -0300

    Update model.js

--- js/model.js
+++ js/model.js
@@ -1,18 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    currentLang = localStorage.getItem('siteLanguage') || 'en';
-    translatePage();
-    
-    document.querySelectorAll('.skill-content').forEach(content => {
-        const key = content.dataset.i18n;
-        content.dataset.originalPt = translations[key].pt;
-        content.dataset.originalEn = translations[key].en;
-    });
+const LanguageModel = {
+    getCurrentLanguage() {
+        return localStorage.getItem('siteLanguage') || 'en';
+    }
+};
 
-    // Adicionar evento global para delegar o hover
-    document.addEventListener('mouseover', (e) => {
-        if (e.target.classList.contains('highlight-text')) {
-            document.querySelector('.search-bar').value = '';
-            searchText();
-        }
-    });
-});
